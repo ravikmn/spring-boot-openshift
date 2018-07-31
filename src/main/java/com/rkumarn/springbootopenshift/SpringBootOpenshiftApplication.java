9998d9d15a8fbb2f7a1d19c3920130b469395318
Ravi <ravi.kumar_n@nokia.com>
2018-07-31 16:59:45 +0530

    Initial version

---
+++ src/main/java/com/rkumarn/springbootopenshift/SpringBootOpenshiftApplication.java
@@ -0,0 +1,12 @@
+package com.rkumarn.springbootopenshift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOpenshiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOpenshiftApplication.class, args);
+	}
+}
